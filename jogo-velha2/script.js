85544672a9c05181bd1201373d19bd447b0b5b4a
Paulo <120028058+pachecopc@users.noreply.github.com>
2023-07-01 11:52:32 -0300

    atuaçizado

---
+++ jogo-velha2/script.js
@@ -0,0 +1,32 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector("[data-winning-message-text]");
+const winningMessage = document.querySelector("[data-winning-message]");
+const restarButton = document.querySelector("[data-restart-button]");
+
+let isCircleTurn;
+
+const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+];
+
+const startGame = () => {
+    isCircleTurn = false
+
+    for (const cell of cellElements) {
+        cell.classList.remove("circle");
+        cell.classList.remove("x");
+        cell.removeEventListener("click", handleClick);
+        cell.addEventListener("click", handleClick, { once: true});
+    }
+
+}
+
+
